22e4f986b16fa16aa3caf9b11c77da4a08d599b1
Egehan Kalayci <egehankalayci@icloud.com>
2023-08-21 02:27:19 +0300

    -

--- Source/PopupView.swift
+++ Source/PopupView.swift
@@ -459,7 +459,7 @@ public struct Popup<PopupContent: View>: ViewModifier {
         }
 
 #if !os(tvOS)
-        let drag = DragGesture()
+        let drag = DragGesture(minimumDistance: 10)
             .updating($dragState) { drag, state, _ in
                 state = .dragging(translation: drag.translation)
             }
